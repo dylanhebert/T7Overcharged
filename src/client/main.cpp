7e3009d46a151cdc0467e6702f428223bda37861
Dylan Hebert <37176117+dylanhebert@users.noreply.github.com>
2025-02-16 13:43:53 -0600

    Add new output messages

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,13 +1,32 @@
 #include <std_include.hpp>
 
+#include <windows.h>
+#include <iostream>
 #include "game/game.hpp"
 #include "havok/hks_api.hpp"
 #include "loader/component_loader.hpp"
+
+void AttachConsoleWindow() {
+	AllocConsole();
+	FILE* fDummy;
+	freopen_s(&fDummy, "CONOUT$", "w", stdout);
+	freopen_s(&fDummy, "CONOUT$", "w", stderr);
+	freopen_s(&fDummy, "CONIN$", "r", stdin);
+	std::cout.clear();
+	std::cerr.clear();
+	std::cin.clear();
+	std::cout << "Console attached to DLL." << std::endl;
+}
 
 extern "C"
 {
 	int __declspec(dllexport) init(lua::lua_State* L)
 	{
+		#ifdef _DEBUG
+		//AttachConsoleWindow();
+		#endif
+		std::cout << "Initializing T7Overcharged..." << std::endl;
+
 		game::minlog.WriteLine("T7Overchared initiating");
 
 		const lua::luaL_Reg T7OverchargedLibrary[] =
@@ -19,11 +38,13 @@ extern "C"
 		if (!component_loader::post_start())
 		{
 			game::Com_Error_("", 0, 0x200u, "Error while loading T7Overcharged components");
+			std::cerr << "Error while loading T7Overcharged components." << std::endl;
 			game::minlog.WriteLine("Error while loading T7Overcharged components");
 			return 0;
 		}
 
 		game::minlog.WriteLine("T7Overchared initiated");
+		std::cout << "T7Overcharged initialized successfully." << std::endl;
 
 		game::LoadDvarHashMap();
 		return 1;
